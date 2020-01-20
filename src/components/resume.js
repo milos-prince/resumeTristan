75507b885db4c6eeb8a3d3e0b2cebaf590919c1d
milos-prince <michael@freedomgopher.com>
2020-01-19 19:50:09 -0500

    v1 complete. finishing for the day.

---
+++ src/components/resume.js
@@ -0,0 +1,88 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import Style from "../styles/global.css"
+import "./layout.css"
+
+class Resume extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      tabIndex: 0
+    }
+    this.setTab = this.setTab.bind(this);
+  }
+
+  setTab(x){
+    this.setState({
+      tabIndex: x
+    });
+  }
+ 
+
+
+  render() {
+    switch (this.state.tabIndex){
+      case 0:
+          return (
+            <div>
+              <div className="toggleRow">
+             <button className="fancyButton activeFancyButton" onClick={() => this.setTab(0)}>Education</button>
+             <button className="fancyButton" onClick={() => this.setTab(1)}>Work History</button>
+             <button className="fancyButton" onClick={() => this.setTab(2)}>Tools</button>
+             </div>
+             <h3>Bachelor of Arts in Philosophy, 2016</h3>
+             <p>University of Saskatchewan. Saskatoon, SK.</p>
+             <h3>Bachelor of Science in Computer Science, 2017</h3>
+             <p>University of Saskatchewan. Saskatoon, SK.</p>
+            </div>
+            
+          );
+      case 1:
+          return (
+            <div><div className="toggleRow">
+              <button className="fancyButton" onClick={() => this.setTab(0)}>Education</button>
+             <button className="fancyButton activeFancyButton" onClick={() => this.setTab(1)}>Work History</button>
+             <button className="fancyButton" onClick={() => this.setTab(2)}>Tools</button>
+             </div>
+             <h4>Web Developer, Jonar; Montreal, QC – March 2019-Present</h4>
+<p>Upon moving to Montreal I took a job with Jonar, a software company specializing in
+ERP software. As the primary web developer, I spearheaded the launch of a new product website for ParagonERP, 
+and relaunched Jonar's existing site using Gatsby/ReactJS.</p>
+<h4>Owner, Freedom Gopher Web Development; Saskatoon, SK. July 2017-March
+2019</h4><p>Shortly after obtaining a Computer Science degree from the University of Saskatchewan, I
+started a small company doing freelance web design/development work. Most of these
+projects were focused on improving or establishing an online presence for small and medium
+sized companies.</p>
+             
+            </div>
+          );
+      default:
+          return (
+            <div>
+              <div className="toggleRow">
+             <button className="fancyButton" onClick={() => this.setTab(0)}>Education</button>
+             <button className="fancyButton" onClick={() => this.setTab(1)}>Work History</button>
+             <button className="fancyButton activeFancyButton" onClick={() => this.setTab(2)}>Tools</button>
+              </div>
+             <h3>Here's a list of tools/technologies I've been using lately:</h3><ul>
+             <li>HTML/CSS/JS</li><li> React, Gatsby, Netlify</li><li> Bootstrap, jQuery, Git</li><li> Wordpress, Squarespace, Photoshop, After Effects</li>
+             </ul>
+            </div>
+          );
+    }
+    
+    
+  }
+};
+
+export default Resume
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Resume from "../components/resume"
 import Style from "../styles/global.css"
 
 import linkedIN from "../images/linkedIn.svg"
@@ -37,15 +38,19 @@ const IndexPage = () => (
     <div className="titleWrapper">
       <h1>Resume</h1><div className="fancyBox"><div className="d-flex"><p>Resume</p><img src={dl} /></div></div>
     </div>
-    <div className="resumeContentSection"></div>
+    <div className="resumeContentSection">
+      <Resume />
+    </div>
   </section>
   <section className="projects">
     <div className="titleWrapper">
-      <h1>Projects</h1><div className="fancyBox"><div className="d-flex"><p>Projects</p><img src={dl} /></div></div>
+      <h1>Projects</h1>
+      {/*<div className="fancyBox"><div className="d-flex"><p>Projects</p><img src={dl} /></div></div>
+      This is for Tristan's resume*/}
     </div>
     <div className="projectsContentSection">
       
-      <div className="quarterBox bg-paragon"><a href="https://www.paragon-erp.com"><div className="a"><h2>paragon-erp.com</h2><p>As an employee of Jonar, I was responsible for building and launching of a new website promoting Jonar's flagship product, ParagonERP. Built using Gatsby, a static site generator for React, and deployed with Netlify.</p></div></a></div>
+      <div className="quarterBox bg-paragon"><a href="https://www.paragon-erp.com"><div className="a"><h2>paragon-erp.com</h2><p>As an employee of Jonar, I was responsible for building and launching of a new website promoting Jonar's flagship product, ParagonERP.<span className="hideOnS"> Built using Gatsby, a static site generator for React, and deployed with Netlify.</span></p></div></a></div>
       <div className="quarterBox bg-jonar"><a href="https://www.jonar.com"><div className="b"><h2>jonar.com</h2><p>After the ParagonERP.com project, Jonar.com was migrated away from Wordpress and rebuilt, again using Gatsby/ReactJS.</p></div></a></div>
       <div className="quarterBox bg-evans"><a href="https://www.janeevans.ca"><div className="c"><h2>janeevans.ca</h2><p>Portfolio site built for artist Jane Evans to showcase her work and methods. Built in Wordpress.</p></div></a></div>
       <div className="quarterBox bg-ufcw"><a href="https://www.ufcw1400.ca"><div className="d"><h2>ufcw1400.ca</h2><p>Information portal built for a local union, the UFCW1400, to host documents and provide updates to members. Built in Wordpress.</p></div></a></div>
