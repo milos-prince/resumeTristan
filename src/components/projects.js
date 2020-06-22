a9b5b20d6b587dd373bb99908b53824e30aea700
milos-prince <michael@freedomgopher.com>
2020-06-22 16:32:33 -0400

    new link

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,7 +17,7 @@ const Projects = () => (
       <div className="quarterBox bg-paragon"><a href="https://www.paragon-erp.com"><h2>paragon-erp.com</h2></a><p className="hideOnS">As an employee of Jonar, I was responsible for building and launching of a new website promoting Jonar's flagship product, ParagonERP.</p></div>
       <div className="quarterBox bg-jonar"><a href="https://www.wikett.com"><h2>wikett.com</h2></a><p className="hideOnS">Product site for Wikett by Jonar, built using React & Gatsby</p></div>
       <div className="quarterBox bg-evans"><a href="https://www.janeevans.ca"><h2>janeevans.ca</h2></a><p className="hideOnS">Portfolio site built for artist Jane Evans to showcase her work and methods.</p></div>
-      <div className="quarterBox bg-ufcw"><a href="https://www.ufcw1400.ca"><h2>ufcw1400.ca</h2></a><p className="hideOnS">Information portal built for a local union, the UFCW1400, to host documents and provide updates to members.</p></div>
+      <div className="quarterBox bg-ufcw"><a href="https://www.lbplaw.ca"><h2>lbplaw.ca</h2></a><p className="hideOnS">Site for advertising legal services, 2020</p></div>
     </div>
 
 )
