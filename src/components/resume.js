06d5e059e961baea3b5dd2fd0f0b167e3d4950d6
milos-prince <mike.pinder@gmail.com>
2020-08-21 13:15:27 -0400

    cleeny

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,16 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import Style from "../styles/global.css"
 import "./layout.css"
 
 class Resume extends React.Component {
