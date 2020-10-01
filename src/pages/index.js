8c93a15e3fa63fb50f835fb8f912a668839fcd25
milos-prince <mike.pinder@gmail.com>
2020-10-01 10:46:53 -0400

    optimizing...

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,16 +25,16 @@ const IndexPage = () => (
   <div className="column">
   <section className="bio">
     <div className="contact">
-      <p className="contactInfo"><a href="mailto:michael@freedomgopher.com">Get in touch</a> or check me out on</p><a href="https://www.linkedin.com/in/michael-pinder-06b05b194/" className="no-ul"><img src={linkedIN} className="my-1"/></a>
+      <p className="contactInfo"><a href="mailto:michael@freedomgopher.com">Get in touch</a> or check me out on</p><a href="https://www.linkedin.com/in/michael-pinder-06b05b194/" className="no-ul"><img src={linkedIN} className="my-1" alt="linkedIN logo"/></a>
     </div>
     <div className="bioMainContent">
-      <div className="photoWrapper"><img src={profile} /></div>
+      <div className="photoWrapper"><img src={profile} alt="Profile picture"/></div>
       <div className="bioWriteup"><p>I'm a (mostly) front end developer born and raised in Saskatchewan, currently living my best life in Montreal, QC. I use a variety of tools to make beautiful and functional things on the internet. I firmly believe that Skyrim belongs to everybody.<br /><br /> I work alongside a fantastic team at <a href="https://www.jonar.com">Jonar</a> that I am very proud to be a part of.</p></div>
     </div>
   </section>
   <section className="resume">
     <div className="titleWrapper">
-      <h1>Resume</h1><div className="fancyBox"><a className="no-ul" href={resume} download><div className="d-flex"><p>Resume</p><img src={dl} /></div></a></div>
+      <h1>Resume</h1><div className="fancyBox"><a className="no-ul" href={resume} download><div className="d-flex"><p>Resume</p><img src={dl} alt="download icon"/></div></a></div>
     </div>
     <div className="resumeContentSection">
       <Resume />
